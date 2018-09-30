c7765af913616e334fb25a9e11ccef3ff3d86321
Andrew Wright <naruhodo007@gmail.com>
2018-09-30 10:20:47 +0000

    Passed tests with Bootstrap

--- run_app.py
+++ run_app.py
@@ -70,4 +70,4 @@ if __name__ == '__main__':
     app.debug = True
     host = os.environ.get('IP', '0.0.0.0')
     port = int(os.environ.get('PORT', 8080))
-app.run(host=host, port=port)
+    app.run(host=host, port=port)
